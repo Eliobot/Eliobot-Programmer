6ba144bb31331eca507cf6fe7a0db89302e44e1d
Romain <romain@boutrois.com>
2024-01-24 17:50:35 +0100

    [FIX] Default main.py

--- code/main.py
+++ code/main.py
@@ -1,13 +1,7 @@
 import board
-from digitalio import DigitalInOut, Direction, Pull
 import elio
 import time
-from analogio import AnalogIn
 import neopixel
-
-# Button declaration
-buttonPin = DigitalInOut(board.IO39)
-buttonPin.direction = Direction.INPUT
 
 # Built in Neopixel declaration
 pixels = neopixel.NeoPixel(
@@ -16,60 +10,27 @@ pixels = neopixel.NeoPixel(
 # Create a colour wheel index int
 color_index = 0
 
-
-motorAIN1 = DigitalInOut(board.IO36)
-motorAIN2 = DigitalInOut(board.IO38)
-motorBIN1 = DigitalInOut(board.IO35)
-motorBIN2 = DigitalInOut(board.IO37)
-motorAIN1.direction = Direction.OUTPUT
-motorAIN2.direction = Direction.OUTPUT
-motorBIN1.direction = Direction.OUTPUT
-motorBIN2.direction = Direction.OUTPUT
-
-
-while buttonPin.value == True:
-    # Get the R,G,B values of the next colour
-    r, g, b = elio.rgb_color_wheel(color_index)
-    # Set the colour on the NeoPixel
-    pixels[0] = (r, g, b, 0.5)
-    pixels.show()
-    # Increase the wheel index
-    color_index += 1
-
-    # Sleep for 15ms so the colour cycle isn't too fast
-    time.sleep(0.015)
-
+speed = 100
 
 
 while True:
-    if elio.get_obstacle(1):
+    if elio.getObstacle(1):
         pixels.fill((255, 0, 0))
         pixels.show()
-        motorAIN1.value = True
-        motorAIN2.value = False
-        motorBIN1.value = True
-        motorBIN2.value = False
+        elio.moveBackward(speed)
 
         time.sleep(0.2)
 
-        motorAIN1.value = True
-        motorAIN2.value = False
-        motorBIN1.value = False
-        motorBIN2.value = True
+        elio.turnRight(speed)
 
         time.sleep(0.45)
 
-        motorAIN1.value = True
-        motorAIN2.value = True
-        motorBIN1.value = True
-        motorBIN2.value = True
+        elio.motorStop()
 
         time.sleep(0.2)
 
     else:
         pixels.fill((0, 255, 0))
         pixels.show()
-        motorAIN1.value = False
-        motorAIN2.value = True
-        motorBIN1.value = False
-        motorBIN2.value = True
+        
+        elio.moveForward(speed)
